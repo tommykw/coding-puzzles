942249e04df7ff9ad5b0d6578af52a314a69a917
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-01-25 21:05:42 +0900

    Add day01

---
+++ advent-code/day01.hs
@@ -0,0 +1,12 @@
+module Main where
+
+solve :: String -> Int
+solve = sum . map mapPar
+
+mapPar :: Char -> Int
+mapPar '(' = 1
+mapPar ')' = -1
+mapPar _ = 0
+
+main :: IO ()
+main = print =<< solve <$> getLine
