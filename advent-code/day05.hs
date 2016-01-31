7cda4f26b8385a6bf59ba71ac6a1e133e8bb13f4
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-01-31 11:39:50 +0900

    Done implements day05

--- advent-code/day05.hs
+++ advent-code/day05.hs
@@ -12,4 +12,47 @@ import System.Environment (getArgs)
 (<&&>) = liftA2 (&&)
 {-E INLINE (<&&>) #-}
 
--- not implements
+vowels :: S.Set Char
+vowels = S.formList "aeiou"
+
+pairs :: forall a. [a] -> [[a]]
+pairs (x0:x1:xs) = [x0, x1] : pairs (x1:xs)
+pairs _ = []
+
+badSequences :: S.Set String
+badSequences = S.fromList ["ab", "cd", "pq", "xy"]
+
+hasEnoughVowels :: String -> Bool
+hasEnoughVowels = (>= 3) . length . filter (`S.member` vowels)
+
+hasTwiceInARow :: String -> Bool
+hasTwiceInARow = any ((>= 2) . length) . group
+
+hasBadSequence :: String -> Bool
+hasBadSequence = any (`S.member` badSequences) . pairs
+
+hasNonOverlappingPair :: String -> Bool
+hasNonOverlappingPair = go . pairs
+  where
+    go :: [String] -> Bool
+    go (x0:x1:xs) = x0 `elem` xs || go (x1:xs)
+    go _ = False
+
+hasSeparatedRepetition :: String -> Bool
+hasSeparatedRepetition (x0:x1:x2:xs) = x0 == x2 || hasSeparatedRepetition (x1:x2:x2)
+hasSeparatedRepetioton _ = False
+
+isNice :: String -> Bool
+isNice = hasEnoughVowels <&&> hasTwiceInARow <&&> (not . hasBadSequence)
+
+isNice' :: String -> Bool
+isNice' = hasNonOverlappingPair <&&> hasSeparatedRepetition
+
+main :: IO ()
+main = do
+  [arg] <- getArgs
+  let run fn = print =<< length <$> filter (== True) <$> fmap fn <$> lines <$> getContents
+  case arg of
+    "1" -> run isNice
+    "2" -> run isNice'
+    _   -> error "Provide either 1 or 2 as argument."
