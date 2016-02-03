49e1e188513a6cd9081de15ad734d76339d08f59
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-02-03 13:47:58 +0900

    [WIP] implements

--- advent-code/day06.hs
+++ advent-code/day06.hs
@@ -19,3 +19,39 @@ import qualified Text.Megaparsec.Lexer as L
 
 type Coord = (Integer, Integer)
 type Coords = S.Set Coord
+
+data Op = TurnOn Coords | TurnOff Coords | Toggle Coords
+  deriving (Show, Eq)
+
+opsParser :: Parser [Op]
+opsParser = many (opParser <* eol) <* eof
+
+opParser :: Parser [Op]
+opParser = try (opP "turn off" TurnOff)
+    <|> try (opP "turn on" TurnOn)
+    <|> opP "toggle" Toggle
+  where 
+    coordTupleP :: Parser Coord
+    coordTupleP = 
+      (,) <$> L.integer
+          <* char ','
+          <*> L.integer
+
+    coordsP :: Parser (Coord, Coord)
+    coordsP = do
+      from <- coordTupleP
+      string " through "
+      to <- coordTupleP
+      return (from, to)
+
+    opP :: String -> (Coords -> r) -> Parser r
+    opP s f = do
+      string s
+      space
+      (from, to) <- coordsP
+      return $ f $ expand from to
+
+    expand :: Coord -> Coord -> Coords
+    expand (fx, fy) (tx, ty) = $.fromList [(x, y) | x <- [fx..tx], y <- [fy..ty]]
+
+-- not implements
