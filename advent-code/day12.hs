148ccc498b5d2e286d2b540f589dc4ea34d45151
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-02-05 07:50:15 +0900

    Done implemenets day12

--- advent-code/day12.hs
+++ advent-code/day12.hs
@@ -17,4 +17,20 @@ parse s = run <$> eitherDecode s
     run (Number n) = [n]
     run _ = pure 0
 
+parse' :: BS.ByteString -> Either String [Scientific]
+parse' s = run <$> eitherDecode s
+  where 
+    run (Object o) = if "red" `elem` M.elems o then pure 0 else concatMap run o
+    run (Array a) = concatMap run a
+    run (Number n) = pure n
+    run _ = pure 0
 
+main :: IO ()
+main = do
+  [c] <- getArgs
+  contents <- BS.getContents
+
+  print $ sum <$> case c of
+    "1" -> parse contents
+    "2" -> parse' contents
+    _   -> error "Specify 1 or 2"
