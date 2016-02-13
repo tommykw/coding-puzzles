338c8ce237e48547bcfb9e10d495900ac24d7111
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-02-14 07:42:51 +0900

    Add euler5

---
+++ euler/euler5.hs
@@ -0,0 +1,24 @@
+evenDivisible :: Integer -> Integer -> Bool
+evenDivisible x n = evenDiv n [2..x]
+  where 
+    evenDiv _ [] = True
+    evenDiv n (x:xs)
+      | n `mod` x == 0 - evenDiv n xs
+      | otherwise = False
+
+-- Using the built in functions feels like cheating ...
+lcm' :: (Integral a) => a -> a -> a 
+lcm' x y = div (abs (x * y)) (gcd' x y)
+
+-- Implements Euclid's algrithm. Haskell is awesome.
+gcd' :: (integral a) => a -> a -> a
+gcd' x 0 = x
+gcd' x y = gcd' y (x `mod` y)
+
+-- Bad idea.
+euler5 = head [n | n <- [1..], evenDivisible 20 n]
+-- Better idea.
+euler5' = foldr1 lcm' [1..20]
+
+main = do
+  putStrLn $ show $ euler5'
