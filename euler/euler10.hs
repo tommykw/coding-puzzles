ce1d869c3c3451e894eda404bbb90bab7c937ca5
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-02-18 05:58:43 +0900

    Add euler10

---
+++ euler/euler10.hs
@@ -0,0 +1,13 @@
+primes = 2 : 3 : filter ((==1) . length . primeFactors) [5,7..]
+
+primeFactors n = factor n primes
+  where 
+    factor n (p:os)
+      | p * p > n = [n]
+      | n `mod` p == 0 = p : factor (n `div` p) (p:ps)
+      | otherwise = factor n ps
+
+euler10 = sum $ takeWhile (<2000000) primes
+
+main = do
+  putStrLn $ show $ euler10
